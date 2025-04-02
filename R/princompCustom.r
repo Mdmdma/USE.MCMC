03583ca73df89a5c3fbb83742425c3718efd6a45
mathis <m.erler@gmx.ch>
2025-04-02 11:54:26 +0200

    resolve warings and notes

--- R/princompCustom.r
+++ R/princompCustom.r
@@ -1,16 +1,16 @@
 #' Custom version of princomp
 #' The warning() at L53 substitutes the stop() in the original version of "princomp".
-#' @param formula a formula with no response variable, referring only to numeric variables.
-#' @param data an optional data frame (or similar: see model.frame) containing the variables in the formula formula. By default the variables are taken from environment(formula).
+#' param formula a formula with no response variable, referring only to numeric variables.
+#' param data an optional data frame (or similar: see model.frame) containing the variables in the formula formula. By default the variables are taken from environment(formula).
 #' @param subset an optional vector used to select rows (observations) of the data matrix x.
-#' @param na.action a function which indicates what should happen when the data contain NAs. The default is set by the na.action setting of options, and is na.fail if that is unset. The ‘factory-fresh’ default is na.omit.
+#' param na.action a function which indicates what should happen when the data contain NAs. The default is set by the na.action setting of options, and is na.fail if that is unset. The ‘factory-fresh’ default is na.omit.
 #' @param x a numeric matrix or data frame which provides the data for the principal components analysis.
 #' @param cor a logical value indicating whether the calculation should use the correlation matrix or the covariance matrix. (The correlation matrix can only be used if there are no constant variables.)
 #' @param scores a logical value indicating whether the score on each principal component should be calculated.
 #' @param covmat a covariance matrix, or a covariance list as returned by cov.wt (and cov.mve or cov.mcd from package MASS). If supplied, this is used rather than the covariance matrix of x.
 #' @param fix_sign Should the signs of the loadings and scores be chosen so that the first element of each loading is non-negative?
-#' @param object Object of class inheriting from "princomp".
-#' @param newdata An optional data frame or matrix in which to look for variables with which to predict. If omitted, the scores are used. If the original fit used a formula or a data frame or a matrix with column names, newdata must contain columns with the same names. Otherwise it must contain the same number of columns, to be used in the same order.
+#' param object Object of class inheriting from "princomp".
+#' param newdata An optional data frame or matrix in which to look for variables with which to predict. If omitted, the scores are used. If the original fit used a formula or a data frame or a matrix with column names, newdata must contain columns with the same names. Otherwise it must contain the same number of columns, to be used in the same order.
 #' @importFrom stats cov.wt setNames
 #' @keywords internal
 #' @export
@@ -18,17 +18,17 @@ princompCustom <- function (x, cor = FALSE, scores = TRUE, covmat = NULL, subset
   chkDots(...)
   cl <- match.call()
   cl[[1L]] <- as.name("princomp")
-  z <- if (!missing(x)) 
+  z <- if (!missing(x))
     as.matrix(x)[subset, , drop = FALSE]
   if (is.list(covmat)) {
-    if (anyNA(match(c("cov", "n.obs"), names(covmat)))) 
+    if (anyNA(match(c("cov", "n.obs"), names(covmat))))
       stop("'covmat' is not a valid covariance list")
     cv <- covmat$cov
     n.obs <- covmat$n.obs
     cen <- covmat$center
   }
   else if (is.matrix(covmat)) {
-    if (!missing(x)) 
+    if (!missing(x))
       warning("both 'x' and 'covmat' were supplied: 'x' will be ignored")
     cv <- covmat
     n.obs <- NA
@@ -36,7 +36,7 @@ princompCustom <- function (x, cor = FALSE, scores = TRUE, covmat = NULL, subset
   }
   else if (is.null(covmat)) {
     dn <- dim(z)
-    if (dn[1L] < dn[2L]) 
+    if (dn[1L] < dn[2L])
       stop("'princomp' can only be used with more units than variables")
     covmat <- stats::cov.wt(z)
     n.obs <- covmat$n.obs
@@ -44,31 +44,31 @@ princompCustom <- function (x, cor = FALSE, scores = TRUE, covmat = NULL, subset
     cen <- covmat$center
   }
   else stop("'covmat' is of unknown type")
-  if (!is.numeric(cv)) 
+  if (!is.numeric(cv))
     stop("PCA applies only to numerical variables")
   if (cor) {
     sds <- sqrt(diag(cv))
-    if (any(sds == 0)) 
+    if (any(sds == 0))
       stop("cannot use 'cor = TRUE' with a constant variable")
     cv <- cv/(sds %o% sds)
   }
   edc <- eigen(cv, symmetric = TRUE)
   ev <- edc$values
   if (any(neg <- ev < 0)) {
-    if (any(ev[neg] < -9 * .Machine$double.eps * ev[1L])) 
+    if (any(ev[neg] < -9 * .Machine$double.eps * ev[1L]))
       warning("covariance matrix is not non-negative definite")
     else ev[neg] <- 0
   }
   cn <- paste0("Comp.", 1L:ncol(cv))
   names(ev) <- cn
-  dimnames(edc$vectors) <- if (missing(x)) 
+  dimnames(edc$vectors) <- if (missing(x))
     list(dimnames(cv)[[2L]], cn)
   else list(dimnames(x)[[2L]], cn)
   sdev <- sqrt(ev)
-  sc <- stats::setNames(if (cor) 
+  sc <- stats::setNames(if (cor)
     sds
     else rep.int(1, ncol(cv)), colnames(cv))
-  fix <- if (fix_sign) 
+  fix <- if (fix_sign)
     function(A) {
       mysign <- function(x) ifelse(x < 0, -1, 1)
       A[] <- apply(A, 2L, function(x) x * mysign(x[1L]))
@@ -76,23 +76,23 @@ princompCustom <- function (x, cor = FALSE, scores = TRUE, covmat = NULL, subset
     }
   else identity
   ev <- fix(edc$vectors)
-  scr <- if (scores && !missing(x) && !is.null(cen)) 
+  scr <- if (scores && !missing(x) && !is.null(cen))
     scale(z, center = cen, scale = sc) %*% ev
-  if (is.null(cen)) 
+  if (is.null(cen))
     cen <- rep(NA_real_, nrow(cv))
-  edc <- list(sdev = sdev, loadings = structure(ev, class = "loadings"), 
-              center = cen, scale = sc, n.obs = n.obs, scores = scr, 
+  edc <- list(sdev = sdev, loadings = structure(ev, class = "loadings"),
+              center = cen, scale = sc, n.obs = n.obs, scores = scr,
               call = cl)
   class(edc) <- "princomp"
   edc
 }
 
-pca_predict <- function (data, model, nPC) 
+pca_predict <- function (data, model, nPC)
 {
     predict(data, model)[, 1:nPC]
 }
 
-rastPCA <- function (env.rast, nPC = NULL, naMask = TRUE, stand = FALSE) 
+rastPCA <- function (env.rast, nPC = NULL, naMask = TRUE, stand = FALSE)
 {
   if (inherits(env.rast, "BasicRaster")) {
     env.rast <- terra::rast(env.rast)
@@ -105,10 +105,10 @@ rastPCA <- function (env.rast, nPC = NULL, naMask = TRUE, stand = FALSE)
   }
   if (nPC > terra::nlyr(env.rast)) {
     nPC <- terra::nlyr(env.rast)
-    message(paste0("\nThe maximum number of PCs that can be estimated is ", 
+    message(paste0("\nThe maximum number of PCs that can be estimated is ",
                    terra::nlyr(env.rast), "\n"))
   }
-  if (sum(terra::global(env.rast, fun = "isNA")) == sum(terra::global(env.rast, 
+  if (sum(terra::global(env.rast, fun = "isNA")) == sum(terra::global(env.rast,
                                                                       fun = function(x) terra::ncell(x)))) {
     stop("The layers are empty or contain only NAs")
   }
