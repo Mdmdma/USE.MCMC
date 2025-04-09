6a81f0c315b1d6f23a308ad09f818c50219d9fbf
mathis <m.erler@gmx.ch>
2025-04-09 10:46:02 +0200

    simplify the function that adds the random step

--- R/addHighDimGaussian.R
+++ R/addHighDimGaussian.R
@@ -7,14 +7,14 @@
 #' @param mean_vec vector of the means of the Gaussian to be added
 #' @param cov_mat covariance matrix of the Gaussian to be added
 #'
-#' @returns a function that takes a point given as a dataframe as input and returns it with gaussian noise added to the specified dimensions
+#' @returns a function that takes a point given as a dataframe as input and returns it with Gaussian noise added to the specified dimensions
 #' @export
-#' @importFrom dplyr %>%
 #'
 addHighDimGaussian <- function(dim = 0, mean_vec = matrix(0, ncol = dim), cov_mat = diag(dim)){
   addedHighDimGaussian <- function(currentPoint, dim = ""){
-    randomVector <- mvtnorm::rmvnorm(1, mean = mean_vec, sigma = cov_mat) %>% c(0)
-    currentPoint <- currentPoint %>% dplyr::mutate(dplyr::across(dplyr::all_of(dim), ~ . + randomVector[match(dplyr::cur_column(), dim)]))
+    randomVector <- mvtnorm::rmvnorm(1, mean = mean_vec, sigma = cov_mat)
+    dimToAdd <- sf::st_drop_geometry(currentPoint[dim])
+    nextPoint[dim] <- dimToAdd + randomVector
   }
 
 }
