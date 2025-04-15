d8305ce9b68f2d9dc607890ff4d78db40db3c369
mathis <m.erler@gmx.ch>
2025-04-15 14:52:29 +0200

    implement burn in with adaptive covariance

--- R/mcmcSampling.R
+++ R/mcmcSampling.R
@@ -16,9 +16,11 @@ mcmcSampling <- function(dataset = NULL, dimensions= list(""), densityFunction =
   current.point <- dataset[starting.index, ]
   # burn in
   if(burnin) {
+    cat("Burn in\n")
     covariance.correction <- 1
     points.rejected <- 0
     num.burnin.samples <- 400
+    pb.burnin <- utils::txtProgressBar(min = 0, max = num.burnin.samples, style = 3)
    while (points.rejected / num.burnin.samples < 0.2| points.rejected / num.burnin.samples > 0.3) {
       # the numbers of the condition depend on the exact threshold, Gelman, Roberts, and Gilks (1996) proposes 0.23 was optimal
       points.rejected <- 0
@@ -28,12 +30,14 @@ mcmcSampling <- function(dataset = NULL, dimensions= list(""), densityFunction =
           current.point <- proposed.point
         }
         else points.rejected <- points.rejected + 1
+        utils::setTxtProgressBar(pb.burnin, i)
+        cat("\rCurrent rejection ratio: ", points.rejected / i)
       }
       if (points.rejected / num.burnin.samples < 0.2) covariance.correction <- covariance.correction * stats::rnorm(1, mean = 1.3, sd = 0.1)
       if (points.rejected /num.burnin.samples > 0.3) covariance.correction <- covariance.correction * stats::rnorm(1, mean = 0.7, sd = 0.1)
       cat("\rThe current rejection rate is", points.rejected /num.burnin.samples, ", the currend covariance adjustment factor is ", covariance.correction)
+      cat("\nThe final covariance correction is ", covariance.correction, "\n")
     }
-    cat("\nThe final covariance correction is ", covariance.correction, "\n")
   }
   sampled.points <- dataset[0, ]
   points.rejected <- 0
