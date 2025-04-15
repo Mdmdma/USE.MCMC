98641433c9924bb935fb07b033d7e1e3595a05c5
mathis <m.erler@gmx.ch>
2025-04-15 18:07:06 +0200

    optimising code to eliminate computing the density twice per point

--- R/acceptNextPoint.R
+++ R/acceptNextPoint.R
@@ -2,15 +2,15 @@
 #'
 #' \code{acceptNextPoint} checks whether the proposed point should be accepted
 #'
-#' @param current.point Current state of the markov chain, given as a sf dataframe.
-#' @param proposed.point Proposed next state of the markov chain, given as a sf dataframe.
-#' @param densityFunction Function that can take a point given as a sf dataframe as a input and returns the target density at that location.
+#' @param current.point Current state of the markov chain, given as a sf dataframe with a column called $density containing the value of the target density.
+#' @param proposed.point Proposed next state of the markov chain, given as a sf dataframe with a column called $density containing the value of the target density.
 #'
 #' @returns TRUE if the proposed point should be accepted
 #' @keywords internal
 #'
-acceptNextPoint <- function(current.point, proposed.point, densityFunction){
-  acceptance.ratio <- densityFunction(proposed.point) / densityFunction(current.point)
+acceptNextPoint <- function(current.point, proposed.point){
+  acceptance.ratio <- proposed.point$density / current.point$density
+  if (acceptance.ratio > 1) return(TRUE)
   if (stats::runif(1) < acceptance.ratio) return(TRUE)
   return(FALSE)
 }
