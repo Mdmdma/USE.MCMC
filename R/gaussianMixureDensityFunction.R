3703469ed94357fc78c169f9a822030e6682ec6f
mathis <m.erler@gmx.ch>
2025-04-15 14:43:45 +0200

    fix bug in environmental thresholding

--- R/gaussianMixureDensityFunction.R
+++ R/gaussianMixureDensityFunction.R
@@ -9,7 +9,7 @@ gaussianMixtureDensityFunction <- function(env.data, dim = "", threshold = 0.01)
   densityAtPointEstimator <- function(point){
     point <- as.matrix(sf::st_drop_geometry(point[dim]))
     density <- mclust::predict.densityMclust(environmental.data.model, point)
-    if (density < threshold ) return(0)
+    if (density < threshold ) return(threshold/ 1000)
     return(1)
   }
   return(densityAtPointEstimator)
--- R/mclustDensityFunction.R
+++ R/mclustDensityFunction.R
@@ -13,7 +13,7 @@ mclustDensityFunction <- function(env.model = NULL, presence.model = NULL, dim =
   densityAtPointEstimator <- function(point){
     point <- as.matrix(sf::st_drop_geometry(point[dim]))
     density <- mclust::predict.densityMclust(env.model, point)
-    if (density < threshold ) return(0)
+    if (density < threshold ) return(threshold / 1000)
     return(max(0, 1 - mclust::predict.densityMclust(presence.model, point)  * 10)) # this approach is dubeaous
     }
   return(densityAtPointEstimator)
