4d25d3d9dad63421a6f9fd1167321329b12f994d
mathis <m.erler@gmx.ch>
2025-04-23 15:03:35 +0200

    cluster setup

--- development_scripts/cluster_setup.sh
+++ development_scripts/cluster_setup.sh
@@ -4,5 +4,5 @@
 module load eth_proxy
 
 #load R and needed functionalities
-module load imodule load stack/2024-06 r/4.4.0 cmake/3.27.7 udunits/2.2.28 openssl/3.1.3-zhfub4o gdal/3.4.3 geos/3.9.1 proj/9.2.1 sqlite/3.43.2
+module load stack/2024-06 r/4.4.0 cmake/3.27.7 udunits/2.2.28 openssl/3.1.3-zhfub4o gdal/3.4.3 geos/3.9.1 proj/9.2.1 sqlite/3.43.2
 
