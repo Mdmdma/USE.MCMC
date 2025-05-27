9401c951e00c54ebe40c65da1ac35e072075e7b5
mathis <m.erler@gmx.ch>
2025-05-27 14:04:29 +0200

    update script to be able to runn different seeds on different nodes

--- development_scripts/cluster_comp/cluster_setup.sh
+++ development_scripts/cluster_comp/cluster_setup.sh
@@ -7,3 +7,5 @@ module load eth_proxy
 module load stack/2024-06 r/4.4.0 cmake/3.27.7 udunits/2.2.28 openssl/3.1.3-zhfub4o gdal/3.4.3 geos/3.9.1 proj/9.2.1 sqlite/3.43.2
 
 # srun --ntasks=1 --cpus-per-task=20 --time=4:00:00 --mem-per-cpu=1024 --x11 --pty bash
+# sbatch --ntasks=1 --cpus-per-task=20 --time=4:00:00 --mem-per-cpu=1024 Rscript development_scripts/cluster_comp/cluster_setup.sh                                                                                                                         │** testing if installed package can be loaded from final location
+Rscript Rscript /cluster/home/merler/USE.MCMC/development_scripts/cluster_comp/distributed_precomputation.R --seed 42
