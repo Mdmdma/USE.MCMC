c53f1b808b02b6190c847460e562048328de96ae
mdmdma <m.erler@gmx.ch>
2026-02-17 15:04:40 +0100

    update last plot

--- development_scripts/demonstrate_nn_Uniform_sampling.R
+++ development_scripts/demonstrate_nn_Uniform_sampling.R
@@ -6,6 +6,7 @@ library(ggplot2)
 library(parallel)
 library(FNN)
 library(coda)
+library(profvis)
 
 seed.number = 43
 set.seed(seed.number)
@@ -35,14 +36,14 @@ rpc <- rastPCA(env.data.raster,  stand = TRUE)
 env.data.raster.with.pc <- c(env.data.raster, rpc$PCs)
 
 # Attaching the data in the PCA coordinates
-env.with.pc.fs <- rpc$PCs %>%
+env.with.pc.sf <- rpc$PCs %>%
   as.data.frame(xy = TRUE) %>%
   na.omit() %>%
   sf::st_as_sf(coords = c("x", "y")) %>%
   st_join(env.data.sf)
 
 # subsample env space to speed up the process
-env.with.pc.fs.subsampled <- env.with.pc.fs[runif(min(nrow(env.with.pc.fs), 2000) , 1, nrow(env.with.pc.fs)),]
+env.with.pc.sf.subsampled <- env.with.pc.sf[runif(min(nrow(env.with.pc.sf), 2000) , 1, nrow(env.with.pc.sf)),]
 #specify the dimension that should be included in the following analysys
 
 # Generate virtual species
@@ -66,126 +67,120 @@ sampled.points.uniform.p.location <- sf::st_drop_geometry(sampled.points.uniform
   sf::st_as_sf(coords = c("x", "y"))
 sampled.points.uniform.paper <- terra::extract(env.data.raster.with.pc, sampled.points.uniform.p.location)
 
+to.profile <- function(nn.based.exclustion = TRUE){
+  if (nn.based.exclustion){
+    optimalDistanceThresholdNn(env.data = env.with.pc.sf)
+  }
+  paSamplingNn(env.rast = env.data.raster,
+               pres = virtual.presence.points,
+               grid.res = 15, n.tr = 2,
+               n.samples = n.sample.points,
+               #precomputed.pca = rpc,
+               nn.based.presence.exclusion = nn.based.exclustion)
+}
+
+
+profvis({
+  for (i in 1:5) {
+    to.profile(FALSE)
+    }
+  }
+)
+
+profvis({
+  for (i in 1:1) {
+    x <- paSampling(env.rast = env.data.raster,
+                 pres = virtual.presence.points,
+                 grid.res = 15, n.tr = 2)
+  }
+}
+)
+
+
 sampled.points.uniform.nn <- paSamplingNn(env.rast = env.data.raster,
                                           pres = virtual.presence.points,
                                           grid.res = 15, n.tr = 2,
                                           n.samples = n.sample.points)
 
+
+
+
 sampled.points.mcmc <- paSamplingMcmc(env.data.raster = env.data.raster,
                                       pres = virtual.presence.points, precomputed.pca = rpc, environmental.cutof.percentile = 0.001,
                                       num.chains = 1,
                                       num.cores = 1,
+                                      covariance.correction = 35,
                                       chain.length = 10000,
                                       dimensions = dimensions,
                                       n.samples = n.sample.points)
 
 
+env.with.pc.sf.subsampled <- env.with.pc.sf[
+  stats::runif(min(nrow(env.with.pc.sf), 2000) , 1, nrow(env.with.pc.sf)),]
+env.data.cleaned.subsampled <- sf::st_drop_geometry(
+  env.with.pc.sf.subsampled[dimensions])
+environmental.data.model <- mclust::densityMclust(env.data.cleaned.subsampled)
+title("Gaussian mixture fitted to the environment")
+
+selected.dimensions <- c("PC1", "PC2")
+env.with.pc <- env.with.pc.sf %>%
+  sf::st_drop_geometry()
+test.point <- env.with.pc[1, ]
+
+profvis({
+  for (i in 1:1000) {
+    x <- get.nearest.ten.neighbors <- FNN::get.knnx(env.with.pc[selected.dimensions],
+                                               test.point[selected.dimensions],
+                                               k =100)
+  }
+}
+)
 
 
-if(TRUE) {
-  # Create a layout with 3 rows: 2 for plots, 1 for legend
-  plotting.dimensions <- c("PC1", "PC2" , "PC3", "PC4", "PC5")
-  layout(matrix(c(1:(length(plotting.dimensions) + 1)), nrow = length(plotting.dimensions) + 1, byrow = TRUE),
-         heights = c(rep(2, length(plotting.dimensions)) ,1))  # Make legend row shorter
 
-  par(mar = c(4, 4, 5, 2))  # bottom, left, top, right
-
-  plots <- invisible(lapply(plotting.dimensions, function(col) {
-    x_range <- range(
-      c(density(env.with.pc.fs[[col]])$x,
-        density(sampled.points.uniform.nn[[col]])$x,
-        density(sampled.points.mcmc[[col]])$x)
-    )
-    y_range <- range(
-      c(density(env.with.pc.fs[[col]])$y,
-        density(sampled.points.uniform.nn[[col]])$y,
-        density(sampled.points.mcmc[[col]])$y)
-    )
-
-    plot(density(env.with.pc.fs[[col]]),
-         col = "green",
-         main = paste("Density Comparison for", col),
-         xlim = x_range,
-         ylim = y_range,
-         lwd = 2)
-
-    lines(density(virtual.presence.points.pc[[col]]), col = "black", lwd = 2)
-    lines(density(sampled.points.uniform.paper[[col]]), col = "orange", lwd = 2)
-    lines(density(sampled.points.uniform.nn[[col]]), col = "blue", lwd = 2)
-    lines(density(sampled.points.mcmc[[col]]), col = "red", lwd = 2)
-  }))
-
-  # Create an empty plot for the legend
-  par(mar = c(0, 0, 0, 0))  # Remove margins
-  plot.new()
-  legend("center",
-         legend = c("Environment", "Virtual Presence", "Sampled Points uniform",
-                    "Sampled Points uniform nn", "Sampled Points mcmc"),
-         col = c("green", "black", "orange", "blue", "red"),
-         lwd = 2,
-         horiz = FALSE,  # Vertical legend (rows)
-         bty = "n",      # No box around legend
-         cex = 1.5)      # incease text size
+num.neighbors <- 10
+local.nn.based.local.environmental.cutoff <- function(env.with.pc, num.neighbors){
+  at.point.estimator <- function(point){
+    nearest.neighbors.locations <- FNN::get.knnx(env.with.pc[selected.dimensions],
+                                           point[selected.dimensions],
+                                           k=2)
+    mean.dist.to.neighbors <- mean(nearest.neighbors.locations$nn.dist)
+    print(paste("mean dist to neighbors ", mean.dist.to.neighbors))
+    nearest.neighbors <- env.with.pc[nearest.neighbors.locations$nn.index,]
+    stats.of.nn <- FNN::get.knnx(env.with.pc[selected.dimensions],
+                                 nearest.neighbors[selected.dimensions],
+                                 k=num.neighbors)
+    mean.dist.to.neighbors.in.neighborhood <- mean(stats.of.nn$nn.dist[1:2,])
+    print(paste("mean dist in neighbrohood ", mean.dist.to.neighbors.in.neighborhood))
+    if (mean.dist.to.neighbors <= mean.dist.to.neighbors.in.neighborhood * 1.2){
+      output <- 1
+    } else {
+      output <- 0
+    }
+  }
 }
 
+density.function <- local.nn.based.local.environmental.cutoff(env.with.pc = env.with.pc, num.neighbors = 100)
+x <- density.function(test.point)
 
- # x <- analyseAreaInHigherDim(sf::st_drop_geometry(env.with.pc.fs))
-
-library(ggplot2)
-library(gridExtra)
-library(cowplot)  # For get_legend() function
-
-plotting.dimensions <- c("PC1", "PC2", "PC3", "PC4", "PC5")
-line.with <- 0.8
-
-# Create individual plots for each PC dimension
-individual_plots <- lapply(plotting.dimensions, function(col) {
-  ggplot() +
-    geom_density(aes(x = env.with.pc.fs[[col]]), color = "green", size = line.with) +
-    geom_density(aes(x = virtual.presence.points.pc[[col]]), color = "black", size = line.with) +
-    geom_density(aes(x = sampled.points.uniform.paper[[col]]), color = "orange", size = line.with) +
-    geom_density(aes(x = sampled.points.uniform.nn[[col]]), color = "blue", size = line.with) +
-    geom_density(aes(x = sampled.points.mcmc[[col]]), color = "red", size = line.with) +
-    labs(
-      title = paste("Density Comparison for", col),
-      x = "Value",
-      y = "Density"
-    ) +
-    theme_minimal() +
-    theme(
-      plot.title = element_text(size = 12, face = "bold"),
-      axis.text = element_text(size = 10),
-      axis.title = element_text(size = 11),
-      legend.position = "none"
-    )
-})
-
-# Create shared legend
-legend <- get_legend(
-  plot <- ggplot() +
-    geom_density(aes(x = c(1,2,3), color = "Environment"), size = line.with) +
-    geom_density(aes(x = c(1,2,3), color = "Virtual Presence"), size = line.with) +
-    geom_density(aes(x = c(1,2,3), color = "Sampled Points Uniform"), size = line.with) +
-    geom_density(aes(x = c(1,2,3), color = "Sampled Points Uniform NN"), size = line.with) +
-    geom_density(aes(x = c(1,2,3), color = "Sampled Points MCMC"), size = line.with) +
-    scale_color_manual(
-      name = "",
-      values = c("Environment" = "green",
-                 "Virtual Presence" = "black",
-                 "Sampled Points Uniform" = "orange",
-                 "Sampled Points Uniform NN" = "blue",
-                 "Sampled Points MCMC" = "red")
-    ) +
-    theme(legend.position = "bottom",
-          legend.text = element_text(size = 12),
-          legend.title = element_blank())
+gridsize <- 30
+grid <- expand.grid(
+  x = seq(min(env.with.pc$PC1), max(env.with.pc$PC1), length.out = gridsize),
+  y = seq(min(env.with.pc$PC2), max(env.with.pc$PC2), length.out = gridsize)
 )
+names(grid) <- c("PC1", "PC2")
 
-# Arrange plots in grid with legend at bottom
-grid.arrange(
-  arrangeGrob(grobs = individual_plots, ncol = 2),
-  legend,
-  heights = c(0.85, 0.15)
-)
+grid$density <- purrr::map_dbl(1:nrow(grid), ~ density.function(grid[.x, , drop = FALSE]))
+
+p.density.naive <- ggplot() +
+  geom_tile(data=grid, aes(x=PC1, y=PC2, fill=density)) +
+  geom_point(data=env.with.pc, aes(x=PC1, y=PC2), size = 0.1, alpha=0.2) +
+  theme_minimal() +
+  labs(title = "Density function in the naive case")
+print(p.density.naive)
+
+
+
+
 
 
